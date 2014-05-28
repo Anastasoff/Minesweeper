c156f79933a82750eface3f49ca96a8866e075a3
Martin Anastasov <m4rt00@gmail.com>
2014-05-28 20:58:54 +0300

    Add Source solution

---
+++ Source/Minesweeper/Човек.cs
@@ -0,0 +1,15 @@
+﻿namespace Mini
+{
+    internal class Човек
+    {
+        public string Name { get; set; }
+
+        public int Score { get; set; }
+
+        public Човек(string name, int score)
+        {
+            this.Name = name;
+            this.Score = score;
+        }
+    }
+}
