de5163fa170d88b2e6beb9e544173ed7f169376c
theexar <easlavov@gmail.com>
2014-07-14 12:28:31 +0300

    Moved al lcommand processing methods to the CommandProcessor class. The CommandProcessor class is no longer static. New fields in the CommandProcessor, moved from the Engine - and instance of GameBorad and ScoreBoard.

--- Source/Minesweeper/Engine/CommandProcessor.cs
+++ Source/Minesweeper/Engine/CommandProcessor.cs
@@ -2,34 +2,86 @@
 {
     using System;
 
-    internal static class CommandProcessor
+    internal class CommandProcessor
     {
+
+        public GameBoard gameboard; // TODO: encapsulate
+        private Scoreboard scoreboard;
         internal static int x { get; set; }
 
         internal static int y { get; set; }
 
         internal static Command command;
 
-        internal static void ReadCommand()
+        public CommandProcessor(GameBoard gameboard, Scoreboard scoreboard)
         {
-            Clear();
-            string commandRead = Console.ReadLine();
-            commandRead = commandRead.Trim();
+            this.gameboard = gameboard;
+            this.scoreboard = scoreboard;
+        }
 
-            switch (commandRead)
+        public void ExecuteCommand(string input)
+        {
+            // exit top restart
+            Command command;
+            try
             {
-                case "exit": command = Command.Exit;
+                command = ParseInput(input);
+            }
+            catch (ArgumentException)
+            {
+                command = Command.ValidMove;
+            }
+            switch (command)
+            {
+                case Command.InvalidMove: ProcessInvalidMove();
                     break;
-                case "top": command = Command.Top;
+                case Command.Exit: ProcessExitCommand();
                     break;
-                case "restart": command = Command.Restart;
+                case Command.Top: ProcessTopCommand();
                     break;
-                default: SetCoordinates(commandRead);
+                case Command.Restart: ProcessRestartCommand();
+                    break;
+                default: ProcessCoordinates();
                     break;
             }
         }
 
-        public static void SetCoordinates(string commandRead)
+        private Command ParseInput(string input)
+        {
+            switch (input)
+            {
+                case "exit": return Command.Exit;
+                case "top": return Command.Top;
+                case "restart": return Command.Restart;
+                default: throw new ArgumentException("Input not a valid command");
+            }
+        }
+
+        private void ProcessInvalidMove()
+        {
+            Console.WriteLine("Illegal command!");
+        }
+
+        private void ProcessTopCommand()
+        {
+            scoreboard.ShowHighScores();
+            CommandProcessor.Clear();
+        }
+
+        private void ProcessExitCommand()
+        {
+            Console.WriteLine("Goodbye!");
+            Environment.Exit(0);
+        }
+
+        private void ProcessRestartCommand()
+        {
+            GameBoard.ResetBoard();
+            ShowWelcomeMessage();
+            gameboard.Display();
+        }
+
+        private static void SetCoordinates(string commandRead) // to private // ************NEW
         {
             string[] point = commandRead.Split(' ');
             if (point.Length != 2)
@@ -51,10 +103,56 @@
             }
         }
 
+        private void ProcessCoordinates() // new function to reduce the mess in the while(true) loop
+        {
+            int x = CommandProcessor.x;
+            int y = CommandProcessor.y;
+
+            if (!gameboard.InBoard(x, y) || gameboard.CellIsRevealed(x, y))
+            {
+                Console.WriteLine("Illegal move!");
+                Console.WriteLine();
+            }
+            else
+            {
+                if (gameboard.HasMine(x, y))
+                {
+                    ShowEndGameMessage(gameboard, scoreboard);
+                    scoreboard.ShowHighScores();
+                    GameBoard.ResetBoard();
+                    ShowWelcomeMessage();
+                    gameboard.Display();
+                }
+                else
+                {
+                    gameboard.RevealBlock(x, y);
+                    gameboard.Display();
+                }
+            }
+        }
+
         internal static void Clear()
         {
             x = 0;
             y = 0;
         }
+
+        public void ShowWelcomeMessage() //transfered from the main class
+        {
+            Console.WriteLine("Welcome to the game “Minesweeper”. Try to reveal all cells without mines. Use 'top' to view the scoreboard, 'restart' to start a new game and 'exit' to quit the game.");
+            Console.WriteLine();
+        }
+
+        public void ShowEndGameMessage(GameBoard board, Scoreboard scoreboard) //transfered from the main class
+        {
+            board.RevealWholeBoard();
+            board.Display();
+            Console.WriteLine("Booooom! You were killed by a mine. You revealed " + board.RevealedCellsCount + " cells without mines.");
+            Console.WriteLine();
+            if (board.RevealedCellsCount > scoreboard.MinInTop5() || scoreboard.Count() < 5)
+            {
+                scoreboard.AddPlayer(board.RevealedCellsCount);
+            }
+        }       
     }
 }
--- Source/Minesweeper/Engine/GameEngine.cs
+++ Source/Minesweeper/Engine/GameEngine.cs
@@ -4,114 +4,31 @@
 
     public class GameEngine
     {
-        private GameBoard gameboard;
-        private Scoreboard scoreboard;
+        private CommandProcessor commandProcessor;
 
         public GameEngine(GameBoard gameboard, Scoreboard scoreboard)// changed both classes' access to internal
-        {
-            this.gameboard = gameboard;
-            this.scoreboard = scoreboard;
+        {           
+            this.commandProcessor = new CommandProcessor(gameboard, scoreboard); // ************NEW
         }
 
         public void Play()
         {
-            ShowWelcomeMessage();
-            gameboard.Display();
+            this.commandProcessor.ShowWelcomeMessage();
+            this.commandProcessor.gameboard.Display();
 
             while (true)
             {
-                ReadCommand();
-                Command command = CommandProcessor.command; //removed the numerous if-statements and replaced them with switch-statement
-                switch (command)
-                {
-                    case Command.InvalidMove: ProcessInvalidMove();
-                        break;
-                    case Command.Exit: ProcessExitCommand();
-                        break;
-                    case Command.Top: ProcessTopCommand();
-                        break;
-                    case Command.Restart: ProcessRestartCommand();
-                        break;
-                    default: ProcessCoordinates();
-                        break;
-                }
+                string input = ReadInput();
+                commandProcessor.ExecuteCommand(input);                
             }
         }
 
-        private void ShowWelcomeMessage() //transfered from the main class
+        private string ReadInput() // new function to reduce the mess in the while(true) loop
         {
-            Console.WriteLine("Welcome to the game “Minesweeper”. Try to reveal all cells without mines. Use 'top' to view the scoreboard, 'restart' to start a new game and 'exit' to quit the game.");
-            Console.WriteLine();
-        }
-
-        private void ShowEndGameMessage(GameBoard board, Scoreboard scoreboard) //transfered from the main class
-        {
-            board.RevealWholeBoard();
-            board.Display();
-            Console.WriteLine("Booooom! You were killed by a mine. You revealed " + board.RevealedCellsCount + " cells without mines.");
-            Console.WriteLine();
-            if (board.RevealedCellsCount > scoreboard.MinInTop5() || scoreboard.Count() < 5)
-            {
-                scoreboard.AddPlayer(board.RevealedCellsCount);
-            }
-        }
-
-        private void ReadCommand() // new function to reduce the mess in the while(true) loop
-        {
-            Console.Write("Enter row and column: ");
-            CommandProcessor.ReadCommand();
-        }
-
-        private void ProcessInvalidMove() // new function to reduce the mess in the while(true) loop
-        {
-            Console.WriteLine("Illegal command!");
-        }
-
-        private void ProcessTopCommand() // new function to reduce the mess in the while(true) loop
-        {
-            scoreboard.ShowHighScores();
-            CommandProcessor.Clear();
-        }
-
-        private void ProcessExitCommand() // new function to reduce the mess in the while(true) loop
-        {
-            Console.WriteLine("Goodbye!");
-            Environment.Exit(0);
-        }
-
-        private void ProcessRestartCommand() // new function to reduce the mess in the while(true) loop
-        {
-            GameBoard.ResetBoard();
-            ShowWelcomeMessage();
-            gameboard.Display();
-        }
-
-        private void ProcessCoordinates() // new function to reduce the mess in the while(true) loop
-        {
-            int x = CommandProcessor.x;
-            int y = CommandProcessor.y;
-
-            if (!gameboard.InBoard(x, y) || gameboard.CellIsRevealed(x, y))
-            {
-                Console.WriteLine("Illegal move!");
-                Console.WriteLine();
-            }
-            else
-            {
-                if (gameboard.HasMine(x, y))
-                {
-                    ShowEndGameMessage(gameboard, scoreboard);
-                    scoreboard.ShowHighScores();
-                    GameBoard.ResetBoard();
-                    ShowWelcomeMessage();
-                    gameboard.Display();
-                }
-                else
-                {
-                    gameboard.RevealBlock(x, y);
-                    gameboard.Display();
-                }
-            }
-        }
+            Console.Write("Enter row and column: "); // TODO: "or another command"
+            string commandRead = Console.ReadLine();
+            commandRead = commandRead.Trim();
+            return commandRead;
+        }                
     }
 }
