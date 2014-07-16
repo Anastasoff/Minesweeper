67abc68e0d2c45e357eed7ba2a11349bca85f2e6
IvaylaH <ivayla_h@yahoo.com>
2014-07-16 02:55:13 -0700

    Added struct Position to keep the coordinates of the Mines Added IMine interface and Mine class

---
+++ Source/Minesweeper/GUI/Position.cs
@@ -0,0 +1,16 @@
+﻿namespace Minesweeper.GUI
+{
+    using System;
+
+    public struct Position
+    {
+        public int row;
+        public int col;
+
+        public Position(int inputRow, int inputCol)
+        {
+            this.row = inputRow;
+            this.col = inputCol;
+        }
+    }
+}
---
+++ Source/Minesweeper/Interfaces/IMine.cs
@@ -0,0 +1,12 @@
+﻿namespace Minesweeper.Interfaces
+{
+    using System;
+    using GUI;
+
+    public interface IMine
+    {
+        char Symbol { get; }
+
+        Position Coordinates { get; }
+    }
+}
