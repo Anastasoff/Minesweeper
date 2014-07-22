e952b7f30b69720018e33574655e24be8ab925ac
Martin Anastasov <m4rt00@gmail.com>
2014-07-22 16:12:56 +0300

    Add only PlayerTest.cs

---
+++ Source/UnitTests/PlayerTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Minesweeper;
+using Minesweeper.Interfaces;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class PlayerTest
+    {
+        [TestMethod]
+        public void TestPlayerNameWithNull()
+        {
+            IPlayer testPlayer = new Player(null, 0);
+
+            Assert.AreEqual("Unnamed player", testPlayer.Name);
+        }
+
+        [TestMethod]
+        public void TestPlayerNameWIthEmptyString()
+        {
+            IPlayer testPlayer = new Player(string.Empty, 0);
+
+            Assert.AreEqual("Unnamed player", testPlayer.Name);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestPlayerNameWithLongString()
+        {
+            IPlayer testPlayer = new Player("This is a long string with 36 chars.", 0);
+        }
+    }
+}
