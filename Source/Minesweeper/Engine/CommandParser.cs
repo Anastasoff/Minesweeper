0bbe6db7fd144076a22d7ee8c4aa0760e5f291e8
theexar <easlavov@gmail.com>
2014-07-26 17:32:54 +0300

    Command parsing has been simplified.

--- Source/Minesweeper/Engine/CommandParser.cs
+++ Source/Minesweeper/Engine/CommandParser.cs
@@ -21,101 +21,176 @@
             this.commands.Add("system", CommandType.System);
         }
 
+        //public Command ExtractCommand(string input, GameBoard gameBoard)
+        //{
+        //    Command command;
+        //    CommandType cmdType;
+        //    Position coordinates;
+
+        //    string[] inputCommands = input.Split(' ');
+        //    if (!IsCommandValid(inputCommands))
+        //    {
+        //        cmdType = CommandType.InvalidInput;
+        //    }
+        //    else
+        //    {
+        //        cmdType = CommandType.ValidMove;
+        //    }
+
+        //    if (inputCommands.Length == 1 || inputCommands.Length == 3)
+        //    {
+        //        try
+        //        {
+        //            cmdType = commands[inputCommands[0]];
+        //        }
+        //        catch (Exception)
+        //        {
+        //            cmdType = CommandType.InvalidInput;
+        //            return command = new Command(cmdType, new Position(0, 0));
+        //        }
+        //    }
+        //    else
+        //    {
+        //        if (!gameBoard.IsInsideBoard(int.Parse(inputCommands[0]), int.Parse(inputCommands[1])))
+        //        {
+        //            cmdType = CommandType.InvalidMove;
+        //        }
+        //    }
+
+        //    int row;
+        //    int col;
+        //    if (cmdType == CommandType.Flag)
+        //    {
+        //        row = int.Parse(inputCommands[1]);
+        //        col = int.Parse(inputCommands[2]);
+        //        coordinates = new Position(row, col);
+        //        if (!gameBoard.IsInsideBoard(row, col))
+        //        {
+        //            cmdType = CommandType.InvalidMove;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        try
+        //        {
+        //            row = int.Parse(inputCommands[0]);
+        //            col = int.Parse(inputCommands[1]);
+        //            coordinates = new Position(row, col);
+        //        }
+        //        catch (Exception)
+        //        {
+        //            coordinates = new Position(0, 0);
+        //        }
+        //    }
+
+        //    command = new Command(cmdType, coordinates);
+        //    return command;
+        //}
+
         public Command ExtractCommand(string input, GameBoard gameBoard)
         {
             Command command;
             CommandType cmdType;
-            Position coordinates;
+            Position coordinates = new Position(0, 0);
 
             string[] inputCommands = input.Split(' ');
-            if (!IsCommandValid(inputCommands))
-            {
-                cmdType = CommandType.InvalidInput;
-            }
-            else
-            {
-                cmdType = CommandType.ValidMove;
-            }
 
-            if (inputCommands.Length == 1 || inputCommands.Length == 3)
+            cmdType = GetCommandType(inputCommands);
+            if (cmdType == CommandType.Flag || cmdType == CommandType.ValidMove)
             {
                 try
                 {
-                    cmdType = commands[inputCommands[0]];
+                    coordinates = GetCoordinates(inputCommands, cmdType);
                 }
                 catch (Exception)
-                {
-                    cmdType = CommandType.InvalidInput;
-                    return command = new Command(cmdType, new Position(0,0));
-                }
-            }
-            else
-            {
-                if (!gameBoard.IsInsideBoard(int.Parse(inputCommands[0]), int.Parse(inputCommands[1])))
                 {
                     cmdType = CommandType.InvalidMove;
                 }
             }
-
-            int row;
-            int col;
-            if (cmdType == CommandType.Flag)
-            {
-                row = int.Parse(inputCommands[1]);
-                col = int.Parse(inputCommands[2]);
-                coordinates = new Position(row, col);
-                if (!gameBoard.IsInsideBoard(row, col))
-                {
-                    cmdType = CommandType.InvalidMove;
-                }
-            }
-            else
-            {
-                try
-                {
-                    row = int.Parse(inputCommands[0]);
-                    col = int.Parse(inputCommands[1]);
-                    coordinates = new Position(row, col);
-                }
-                catch (Exception)
-                {
-                    coordinates = new Position(0, 0);
-                }
-            }
-
 
             command = new Command(cmdType, coordinates);
             return command;
         }
 
-        private bool IsCommandValid(string[] commandElements)
+        private Position GetCoordinates(string[] inputCommands, CommandType cmdType)
         {
-            bool commandResult;
-
-            switch (commandElements.Length)
+            int row;
+            int col;
+            if (cmdType == CommandType.ValidMove)
             {
-                case 1:
-                    commandResult = (commands.ContainsKey(commandElements[0].ToLower())) && (commandElements[0].ToLower() != "flag");
-                    break;
-
-                case 2:
-                    int row = -1;
-                    int col = -1;
-                    bool isValidCoords = int.TryParse(commandElements[0], out row);
-                    commandResult = isValidCoords && int.TryParse(commandElements[1], out col);
-                    break;
-
-                case 3:
-                    string[] coords = new string[2] { commandElements[1], commandElements[2] };
-                    commandResult = (commandElements[0].ToLower() == "flag") && IsCommandValid(coords);
-                    break;
-
-                default:
-                    commandResult = false;
-                    break;
+                row = int.Parse(inputCommands[0]);
+                col = int.Parse(inputCommands[1]);
+            }
+            else
+            {
+                row = int.Parse(inputCommands[1]);
+                col = int.Parse(inputCommands[2]);
             }
 
-            return commandResult;
+            Position coordinates = new Position(row, col);
+            return coordinates;
         }
+
+        private CommandType GetCommandType(string[] inputCommands)
+        {
+            string commandType = inputCommands[0];
+            switch (commandType)
+            {
+                case "flag":
+                    return CommandType.Flag;
+                case "restart":
+                    return CommandType.Restart;
+                case "exit":
+                    return CommandType.Exit;
+                case "top":
+                    return CommandType.Top;
+                default:
+                    return ValidateIfCommandIsValid(inputCommands[0]);
+            }
+        }
+
+        private CommandType ValidateIfCommandIsValid(string input)
+        {
+            int possibleCoordinate;
+            if (int.TryParse(input, out possibleCoordinate))
+            {
+                return CommandType.ValidMove;
+            }
+            else if (this.commands.ContainsKey(input))
+            {
+                return CommandType.ValidMove;
+            }
+            else
+            {
+                return CommandType.InvalidInput;
+            }
+        }
+
+        //private bool IsCommandValid(string[] commandElements)
+        //{
+        //    bool commandResult;
+
+        //    switch (commandElements.Length)
+        //    {
+        //        case 1:
+        //            commandResult = (commands.ContainsKey(commandElements[0].ToLower())) && (commandElements[0].ToLower() != "flag");
+        //            break;
+        //        case 2:
+        //            int row = -1;
+        //            int col = -1;
+        //            bool isValidCoords = int.TryParse(commandElements[0], out row);
+        //            commandResult = isValidCoords && int.TryParse(commandElements[1], out col);
+        //            break;
+        //        case 3:
+        //            string[] coords = new string[2] { commandElements[1], commandElements[2] };
+        //            commandResult = (commandElements[0].ToLower() == "flag") && IsCommandValid(coords);
+        //            break;
+        //        default:
+        //            commandResult = false;
+        //            break;
+        //    }
+
+        //    return commandResult;
+        //}
     }
 }
