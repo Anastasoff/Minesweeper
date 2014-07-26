7fb59b7e37dd11137d8c3e5151294b556467f974
theexar <easlavov@gmail.com>
2014-07-26 12:52:02 +0300

    Added new class - Command.cs

---
+++ Source/Minesweeper/Engine/Command.cs
@@ -0,0 +1,21 @@
+﻿namespace Minesweeper.Engine
+{
+    using GameObjects;
+
+    public class Command
+    {
+        private CommandType commandType;
+        private Position coordinates;
+
+        public Command(CommandType commandType)
+            : this(commandType, new Position(0, 0))
+        {
+        }
+
+        public Command(CommandType commandType, Position coordinates)
+        {
+            this.commandType = commandType;
+            this.coordinates = coordinates;
+        }
+    }
+}
