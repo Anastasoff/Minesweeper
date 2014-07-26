be11de91f1a1e2b0f22c575cfbd3995a0d88b300
theexar <easlavov@gmail.com>
2014-07-26 12:03:29 +0300

    All field-properties have been set back to private fields to match conventions.

--- Source/Minesweeper/Engine/CommandProcessor.cs
+++ Source/Minesweeper/Engine/CommandProcessor.cs
@@ -10,24 +10,24 @@
     public class CommandProcessor
     {
         private const int INITIAL_LIVES = 1;
-        private CommandParser CommandParser { get; set; }
-        private GameBoard GameBoard { get; set; }
-        private Scoreboard ScoreBoard { get; set; }
-        private IOInterface UserIteractor { get; set; }
-        private int RemainingLives { get; set; }
-        private GameBoardMemory CurrentBoardState { get; set; }
+        private CommandParser commandParser;
+        private GameBoard gameBoard;
+        private Scoreboard scoreBoard;
+        private IOInterface userIteractor;
+        private int remainingLives;
+        private GameBoardMemory currentBoardState;        
         private delegate void CellHandler(int row, int col);
 
         public CommandProcessor(GameBoard board, Scoreboard score, IOInterface userIteractor, CommandParser commandParser)
         {
-            this.GameBoard = board;
-            this.ScoreBoard = score;
-            this.UserIteractor = userIteractor;
-            this.CurrentBoardState = new GameBoardMemory();
-            this.CurrentBoardState.Memento = board.SaveMemento();
-            this.CommandParser = commandParser;
-            this.RemainingLives = INITIAL_LIVES;
-        }        
+            this.gameBoard = board;
+            this.scoreBoard = score;
+            this.userIteractor = userIteractor;
+            this.currentBoardState = new GameBoardMemory();
+            this.currentBoardState.Memento = board.SaveMemento();
+            this.commandParser = commandParser;
+            this.remainingLives = INITIAL_LIVES;
+        }
 
         /// <summary>
         /// Handles the execution of all valid commands.
@@ -36,58 +36,54 @@
         public void ExecuteCommand(string input)
         {
             string[] commandsArr = input.Split(' ');
-            Command command = this.CommandParser.ExtractCommand(commandsArr, this.GameBoard);
+            Command command = this.commandParser.ExtractCommand(commandsArr, this.gameBoard);
             switch (command)
             {
                 case Command.InvalidMove:
-                    UserIteractor.ShowMessage("Invalid rows or cols! Try again");
+                    userIteractor.ShowMessage("Invalid rows or cols! Try again");
                     break;
-
                 case Command.Exit:
-                    UserIteractor.ShowMessage("Goodbye!");
+                    userIteractor.ShowMessage("Goodbye!");
                     Environment.Exit(0);
                     break;
-
                 case Command.Top:
-                    ScoreBoard.ShowHighScores();
+                    scoreBoard.ShowHighScores();
                     break;
-
-                case Command.Restart: ProcessRestartCommand();
+                case Command.Restart:
+                    ProcessRestartCommand();
                     break;
-
-                case Command.Flag: ProcessFlagCommand(commandsArr);
+                case Command.Flag:
+                    ProcessFlagCommand(commandsArr);
                     break;
-
                 case Command.InvalidInput:
-                    UserIteractor.ShowMessage("Invalid input! Please try again!");
+                    userIteractor.ShowMessage("Invalid input! Please try again!");
                     break;
-
                 case Command.System:
                     break;
-
-                default: ProcessCoordinates(commandsArr);
+                default:
+                    ProcessCoordinates(commandsArr);
                     break;
             }
         }
 
         private void ShowMessage(string message)
         {
-            this.GameBoard.RevealWholeBoard();
+            this.gameBoard.RevealWholeBoard();
 
-            UserIteractor.DrawBoard(GameBoard.Board);
+            userIteractor.DrawBoard(gameBoard.Board);
 
-            UserIteractor.ShowMessage(message);
-            UserIteractor.ShowMessage(string.Empty);
+            userIteractor.ShowMessage(message);
+            userIteractor.ShowMessage(string.Empty);
         }
 
         private void ShowEndGameMessage()
         {
-            string message = "Booooom! You were killed by a mine. You revealed " + this.GameBoard.RevealedCellsCount + " cells without mines.";
+            string message = "Booooom! You were killed by a mine. You revealed " + this.gameBoard.RevealedCellsCount + " cells without mines.";
             ShowMessage(message);
 
-            if (this.GameBoard.RevealedCellsCount > this.ScoreBoard.MinInTop5() || this.ScoreBoard.Count() < 5)
+            if (this.gameBoard.RevealedCellsCount > this.scoreBoard.MinInTop5() || this.scoreBoard.Count() < 5)
             {
-                this.ScoreBoard.AddPlayer(this.GameBoard.RevealedCellsCount);
+                this.scoreBoard.AddPlayer(this.gameBoard.RevealedCellsCount);
             }
         }
 
@@ -96,7 +92,7 @@
             string message = "Congratulations! You have escaped all the mines and WON the game!";
             ShowMessage(message);
 
-            this.ScoreBoard.AddPlayer(this.GameBoard.RevealedCellsCount);
+            this.scoreBoard.AddPlayer(this.gameBoard.RevealedCellsCount);
         }
 
         private void ProcessFlagCommand(string[] commandsArr)
@@ -104,15 +100,14 @@
             int row = int.Parse(commandsArr[1]);
             int col = int.Parse(commandsArr[2]);
 
-
-            var cellHandler = new CellHandler(GameBoard.PlaceFlag);
+            var cellHandler = new CellHandler(gameBoard.PlaceFlag);
             CheckIfCellIsRevealed(cellHandler, row, col);
         }
 
         private void ProcessRestartCommand()
         {
-            GameBoard.ResetBoard();
-            UserIteractor.DrawBoard(GameBoard.Board);
+            gameBoard.ResetBoard();
+            userIteractor.DrawBoard(gameBoard.Board);
         }
 
         private void ProcessCoordinates(string[] inputCoordinates)
@@ -120,34 +115,34 @@
             int row = int.Parse(inputCoordinates[0]);
             int col = int.Parse(inputCoordinates[1]);
 
-            if (GameBoard.CheckIfHasMine(row, col) && !GameBoard.CheckIfFlagCell(row, col))
+            if (gameBoard.CheckIfHasMine(row, col) && !gameBoard.CheckIfFlagCell(row, col))
             {
                 //Memento logic
-                if (this.RemainingLives > 0)
+                if (this.remainingLives > 0)
                 {
                     bool reverting = AskUserToRevert();
                     if (reverting == true)
                     {
-                        GameBoard.RestoreMemento(CurrentBoardState.Memento);
+                        gameBoard.RestoreMemento(currentBoardState.Memento);
                         return;
                     }
                 }
                 ShowEndGameMessage();
-                GameBoard.ResetBoard();
-                UserIteractor.DrawBoard(GameBoard.Board);
+                gameBoard.ResetBoard();
+                userIteractor.DrawBoard(gameBoard.Board);
             }
-            else if (GameBoard.CheckIfHasMine(row, col) && GameBoard.CheckIfFlagCell(row, col))
+            else if (gameBoard.CheckIfHasMine(row, col) && gameBoard.CheckIfFlagCell(row, col))
             {
                 PrintUsedCellMessage("You've already placed flag at these coordinates! Please enter new cell coordinates!");
             }
             else
             {
-                var cellHandler = new CellHandler(GameBoard.RevealBlock);
+                var cellHandler = new CellHandler(gameBoard.RevealBlock);
                 CheckIfCellIsRevealed(cellHandler, row, col);
-                this.CurrentBoardState.Memento = GameBoard.SaveMemento();
+                this.currentBoardState.Memento = gameBoard.SaveMemento();
             }
 
-            if (GameBoard.CheckIfGameIsWon())
+            if (gameBoard.CheckIfGameIsWon())
             {
                 ShowGameWonMessage();
             }
@@ -155,16 +150,15 @@
 
         private bool AskUserToRevert()
         {
-            string userInput = UserIteractor.GetUserInput("You have one more live. Do you want to revert the board to the previous state?[yes/no]");
+            string userInput = userIteractor.GetUserInput("You have one more live. Do you want to revert the board to the previous state?[yes/no]");
             while (userInput != "yes" && userInput != "no")
             {
-
-                userInput = UserIteractor.GetUserInput("Invalid input! Please enter [yes/no]! ");
+                userInput = userIteractor.GetUserInput("Invalid input! Please enter [yes/no]! ");
             }
 
             if (userInput == "yes")
             {
-                this.RemainingLives--;
+                this.remainingLives--;
                 return true;
             }
 
@@ -173,28 +167,26 @@
 
         private void CheckIfCellIsRevealed(CellHandler cellAction, int row, int col)
         {
-            if (GameBoard.IsCellRevealed(row, col))
+            if (gameBoard.IsCellRevealed(row, col))
             {
                 PrintUsedCellMessage("This cell has already been revealed! Please enter new cell coordinates!");
             }
-            else if (GameBoard.CheckIfFlagCell(row, col))
+            else if (gameBoard.CheckIfFlagCell(row, col))
             {
                 PrintUsedCellMessage("You've already placed flag at these coordinates! Please enter new cell coordinates!");
             }
             else
             {
                 cellAction(row, col);
-                UserIteractor.DrawBoard(GameBoard.Board);
+                userIteractor.DrawBoard(gameBoard.Board);
             }
         }
 
-        
-
         private void PrintUsedCellMessage(string message)
         {
-            UserIteractor.DrawBoard(GameBoard.Board);
-            UserIteractor.ShowMessage(message);
-            UserIteractor.ShowMessage(string.Empty);
+            userIteractor.DrawBoard(gameBoard.Board);
+            userIteractor.ShowMessage(message);
+            userIteractor.ShowMessage(string.Empty);
         }
     }
 }
