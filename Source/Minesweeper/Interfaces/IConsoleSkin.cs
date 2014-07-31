f9f085b1e63e05c0846f4cc1c52491aa43307698
theexar <easlavov@gmail.com>
2014-07-31 15:56:56 +0300

    Added XML documentation

--- Source/Minesweeper/Interfaces/IConsoleSkin.cs
+++ Source/Minesweeper/Interfaces/IConsoleSkin.cs
@@ -3,6 +3,9 @@
     using System;
     using System.Collections.Generic;
 
+    /// <summary>
+    /// Defines properties that allow the extraction of char-color color schemes
+    /// </summary>
     public interface IConsoleSkin
     {
         Dictionary<char, ConsoleColor> ColorScheme { get; }
