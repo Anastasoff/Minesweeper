6e75639b6ac081eabb8b66d09f87e4e73ff0e9f8
theexar <easlavov@gmail.com>
2014-07-31 11:10:45 +0300

    Added XML documentation.

--- Source/Minesweeper/Engine/Command.cs
+++ Source/Minesweeper/Engine/Command.cs
@@ -4,19 +4,34 @@
 
     public class Command
     {
+        /// <summary>
+        /// Creates an instance of the Command type
+        /// </summary>
+        /// <param name="commandType">The type of the command</param>
         public Command(CommandType commandType)
             : this(commandType, new Position(0, 0))
         {
         }
 
+        /// <summary>
+        /// Creates an instance of the Command type
+        /// </summary>
+        /// <param name="commandType">The type of the command</param>
+        /// <param name="coordinates">Position in the 2D plane</param>
         public Command(CommandType commandType, Position coordinates)
         {
             this.CommandType = commandType;
             this.Coordinates = coordinates;
         }
 
+        /// <summary>
+        /// Gets the type of the command
+        /// </summary>
         public CommandType CommandType { get; private set; }
 
+        /// <summary>
+        /// Gets the coordinates of the command
+        /// </summary>
         public Position Coordinates { get; private set; }
     }
 }
