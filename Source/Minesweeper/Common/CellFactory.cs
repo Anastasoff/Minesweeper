413572fe8e5423bd5e659d0762d61c64cad10b67
theexar <easlavov@gmail.com>
2014-07-31 11:03:48 +0300

    Merge

--- Source/Minesweeper/Common/CellFactory.cs
+++ Source/Minesweeper/Common/CellFactory.cs
@@ -4,24 +4,24 @@
     using GameObjects;
 
     /// <summary>
-    /// abstract class that is responsible for creating the different cell types -> Implementation of Abstract Factory Pattern
+    /// Abstract class that is responsible for creating the different cell types -> Implementation of Abstract Factory Pattern
     /// </summary>
     public abstract class CellFactory
     {
         /// <summary>
-        /// creates an instance of the class MineCell
+        /// Creates an instance of the class MineCell
         /// </summary>
-        /// <param name="row">takes integer parameter for the row of the cell</param>
-        /// <param name="col">takes integer parameter for the col of the cell</param>
-        /// <returns>returns an instance of the MineCell class</returns>
+        /// <param name="row">An integer for the row of the cell</param>
+        /// <param name="col">An integer parameter for the col of the cell</param>
+        /// <returns>An instance of the MineCell class</returns>
         public abstract Cell CreateMineCell(Position pos);
 
         /// <summary>
-        /// creates an instance of the class SafeCell
+        /// Creates an instance of the class SafeCell
         /// </summary>
-        /// <param name="row">takes integer parameter for the row of the cell</param>
-        /// <param name="col">takes integer parameter for the col of the cell</param>
-        /// <returns>returns an instance of the SafeCell class</returns>
+        /// <param name="row">An integer for the row of the cell</param>
+        /// <param name="col">An integer for the col of the cell</param>
+        /// <returns>An instance of the SafeCell class</returns>
         public abstract Cell CreateSafeCell(Position pos);
     }
 }
