8dad49e24aa9bd031794571cd7a44badfc5a681e
theexar <easlavov@gmail.com>
2014-07-31 15:26:36 +0300

    Added XML documentation to Position.cs

--- Source/Minesweeper/GameObjects/Position.cs
+++ Source/Minesweeper/GameObjects/Position.cs
@@ -1,10 +1,18 @@
 ﻿namespace Minesweeper.GameObjects
 {
+    /// <summary>
+    /// Holds a pair of values that represent coordinates in a two-dimensional plane
+    /// </summary>
     public struct Position
     {
         public int Row;
         public int Col;
 
+        /// <summary>
+        /// Creates an instance of the Position structure
+        /// </summary>
+        /// <param name="inputRow">The X coordinate</param>
+        /// <param name="inputCol">The Y coordinate</param>
         public Position(int inputRow, int inputCol)
         {
             this.Row = inputRow;
