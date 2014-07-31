eba090b5b72a946a16ed13c0d082b70054c792d0
theexar <easlavov@gmail.com>
2014-07-31 11:57:37 +0300

    GameEngine: some variables renamed (typos). Added xml documentation

--- Source/Minesweeper/Engine/GameEngine.cs
+++ Source/Minesweeper/Engine/GameEngine.cs
@@ -4,27 +4,39 @@
 
     using GUI;
     using Interfaces;
-
+        
+    /// <summary>
+    /// A facade of the game logic
+    /// </summary>
     public class GameEngine
     {
         private CommandProcessor commandProcessor;
-        private IOInterface userIterractor;
+        private IOInterface userInteractor;
         private GameBoard board;
 
-        public GameEngine(CommandProcessor processor, IOInterface iterractor, GameBoard board)
+        /// <summary>
+        /// Creates an instance of the GameEngine class
+        /// </summary>
+        /// <param name="processor">The command processor to be used by the engine</param>
+        /// <param name="interactor">The user interactor to be used by the engine</param>
+        /// <param name="board">The game board to be used by the engine</param>
+        public GameEngine(CommandProcessor processor, IOInterface interactor, GameBoard board)
         {
             this.commandProcessor = processor;
-            this.userIterractor = iterractor;
+            this.userInteractor = interactor;
             this.board = board;
         }
 
+        /// <summary>
+        /// Starts the game
+        /// </summary>
         public void Play()
         {
-            this.userIterractor.ShowWelcomeScreen();
-            this.userIterractor.DrawBoard(this.board.Board);
+            this.userInteractor.ShowWelcomeScreen();
+            this.userInteractor.DrawBoard(this.board.Board);
             while (true)
             {
-                string input = this.userIterractor.GetUserInput("Enter row and column: ");
+                string input = this.userInteractor.GetUserInput("Enter row and column: ");
                 this.commandProcessor.ExecuteCommand(input);
             }
         }
