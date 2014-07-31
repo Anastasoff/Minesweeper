367b778e85421dd4ebcb57293f0894980814b206
Radoslav Chervenkov <chervenkovr@gmail.com>
2014-07-31 11:23:28 +0300

    Added more unit tests

---
+++ Source/UnitTests/GUI/KeyboardInputTest.cs
@@ -0,0 +1,33 @@
+﻿namespace UnitTests.GUI
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Minesweeper.Interfaces;
+    using Minesweeper.GUI;
+    using Moq;
+    using System.IO;
+
+    [TestClass]
+    public class KeyboardInputTest
+    {
+        [TestMethod]
+        public void TestKeyboardInputReturnsRightString()
+        {
+            var input = new StringReader("some text");  
+            Console.SetIn(input);
+            IInputDevice keyboard = new KeyboardInput();
+            var parsedInput = keyboard.GetInput();
+            Assert.AreEqual("some text", parsedInput);
+        }
+
+        [TestMethod]
+        public void TestKeyboardInputTrimsTheInput()
+        {
+            var input = new StringReader("   some text   ");
+            Console.SetIn(input);
+            IInputDevice keyboard = new KeyboardInput();
+            var parsedInput = keyboard.GetInput();
+            Assert.AreEqual("some text", parsedInput);
+        }
+    }
+}
